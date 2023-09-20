44eba71e7a33ea4495339dd00c8608328b03dd8e
Horai <44516902+pensuke81@users.noreply.github.com>
2023-09-20 13:29:30 +0900

    微調整

--- app/routes/_index/FAQ.tsx
+++ app/routes/_index/FAQ.tsx
@@ -8,9 +8,10 @@ import type { FAQSection } from "./get-index-data";
 
 export type FAQProps = FAQSection["data"];
 
-export function FAQ({ items }: FAQProps) {
+export function FAQ({ items, titleCopy }: FAQProps) {
   return (
     <div className="container max-w-5xl pb-28">
+      <h2 className="text-2xl mb-6">{titleCopy}</h2>
       <Accordion type="single" collapsible className="w-full">
         {items.map((item) => (
           <AccordionItem key={item._id} value={item._id}>
